f1f03e1c74233e16fa20df2c4228e87cb745590a
Leland Richardson <leland@tech.pro>
2015-07-11 10:28:59 -0700

    syntax highlighting

--- src/Components/Markdown.js
+++ src/Components/Markdown.js
@@ -1,37 +1,49 @@
-var React = global.React = require('react');
-var md2react = require('md2react');
-// TODO: get highlighting working...
-//var Highlight = require('babel!react-highlight');
+var React = require('react');
+//var md2react = require('md2react');
+var hl = require('highlight.js');
 
-const options = {
-    gfm: true,
-    footnotes: true,
-    sanitize: false,
-    //highlight: function (code, lang, key) {
-    //    return (
-    //        <Highlight
-    //            key={key}
-    //            className={lang}>{code}</Highlight>
-    //    );
-    //}
+
+var marked = require('marked');
+var renderer = new marked.Renderer();
+
+//renderer.heading = function (text, level) {
+//    var escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
+//
+//    return `<h${level} id="${escapedText}">${text}</h${level}`;
+//};
+
+renderer.code = function (code, lang) {
+    return `<pre class="hljs"><code>${hl.highlightAuto(code).value}</code></pre>`;
 };
 
+marked.setOptions({
+    renderer,
+    gfm: true,
+    tables: true,
+    breaks: false,
+    pedantic: false,
+    sanitize: true,
+    smartLists: true,
+    smartypants: false
+});
+
 if (__CLIENT__) require('../Styles/Markdown.less');
+//if (__CLIENT__) require('highlight.js/styles/default.css');
+if (__CLIENT__) require('highlight.js/styles/darkula.css');
 class Markdown extends React.Component {
 
-    //static propTypes = {
-    //    text: React.PropTypes.string.isRequired
-    //}
+    static propTypes = {
+        text: React.PropTypes.string.isRequired
+    }
 
     shouldComponentUpdate(next) {
         return this.props.text !== next.text;
     }
 
     render () {
+        const __html = marked(this.props.text);
         return (
-            <div className="markdown-body">
-                {md2react(this.props.text || "", options)}
-            </div>
+            <div className="markdown-body" dangerouslySetInnerHTML={{ __html }} />
         );
     }
 }
