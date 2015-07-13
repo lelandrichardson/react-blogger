e89ddb0b6dd5f19f8d8f725a66e57421278e66ce
Leland Richardson <leland@tech.pro>
2015-07-13 11:43:45 -0500

    config no longer contains sensitive info

---
+++ config.js
@@ -0,0 +1,10 @@
+
+var config = {
+    domain: "intelligiblebabble.com",
+    title: "Intelligible Babble",
+    keywords: "blogs",
+    description: "",
+    port: 3030
+};
+
+module.exports = config;
